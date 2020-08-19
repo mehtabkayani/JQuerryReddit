f5bd1f3957f68de67bb0787e963872185c2132a8
Mehtab Kayani <mehtab.kayani@se.experis.com>
2020-08-19 16:50:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+    const xhr = $.ajax({
+        url: 'https://www.reddit.com/r/dog/top.json?limit=10',
+        method: 'GET',
+        success: function(data) {
+            const $posts = $('#posts');
+            let postsArray = data.data.children;
+            console.log(length)
+
+            $(postsArray).each((i, post) => {
+            console.log(post)
+                const elPost = `
+                    <div class="post">
+                        <br>
+                        <h5 class="title">${post.data.title}</h5>
+                        <img class="picture" src="${post.data.thumbnail}" alt="">
+                        <br>
+                        <a class="btn btn-sm btn-success" href="${post.data.url_overridden_by_dest}">Click here<a>
+                        <p class="para">Toggling </p>
+                        <hr>
+                    </div>
+                `;
+                
+
+                $posts.append(elPost);
+            });
+
+
+        },
+
+    });
+});
+
+$(document).on('mouseover mouseout', '.picture', function(){        
+    $(this).animate({
+        opacity: 025,
+        right: "+=50",
+        height: "toggle"
+    }, 5000, function (){
+
+    })
+});
+
+
+
+
